47d97d08b451f30f44e9aa61f782f8d06bfc137d
MaherKouki <koukimaher386@gmail.com>
2024-11-27 02:02:04 +0100

    security part

--- src/main/java/com/maher/book/security/UserDetailsServiceImpl.java
+++ src/main/java/com/maher/book/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.maher.book.security;
 
 import com.maher.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,9 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository repository
+    private final UserRepository repository;
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
