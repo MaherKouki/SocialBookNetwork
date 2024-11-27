2a204c68b6d324a0eb89b4de4dd66f949269d1df
MaherKouki <koukimaher386@gmail.com>
2024-11-27 23:53:25 +0100

    register not done yet

--- src/main/java/com/maher/book/auth/AuthenticationService.java
+++ src/main/java/com/maher/book/auth/AuthenticationService.java
@@ -1,13 +1,49 @@
 package com.maher.book.auth;
 
+import com.maher.book.role.RoleRepository;
+import com.maher.book.user.Token;
+import com.maher.book.user.User;
+import com.maher.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public void register(RegistrationRequest request){
 
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(()-> new IllegalStateException("ROLE USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
 
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        //sen email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+
+        return null;
     }
 
 
