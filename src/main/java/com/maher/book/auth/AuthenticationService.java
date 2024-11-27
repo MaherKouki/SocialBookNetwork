8de382593757bb0fdd1514f28a78bbbd49923d96
MaherKouki <koukimaher386@gmail.com>
2024-11-27 23:40:45 +0100

    register not done yet

---
+++ src/main/java/com/maher/book/auth/AuthenticationService.java
@@ -0,0 +1,14 @@
+package com.maher.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public void register(RegistrationRequest request){
+
+
+    }
+
+
+}
