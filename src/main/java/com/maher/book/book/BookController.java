5a6683dd4c0abdcce5fe36856feaf77a0f293133
MaherKouki <koukimaher386@gmail.com>
2024-12-10 23:47:45 +0100

    upload book not completed yet

--- src/main/java/com/maher/book/book/BookController.java
+++ src/main/java/com/maher/book/book/BookController.java
@@ -2,6 +2,7 @@ package com.maher.book.book;
 
 
 import com.maher.book.common.PageResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.websocket.server.PathParam;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("books")
@@ -113,6 +115,19 @@ public class BookController {
     ){
         return ResponseEntity.ok(service.approveReturnBorrowedBook(bookId,connectedUser));
     }
+
+    @PostMapping(value = "/cover/{book-id}" , consumes = "multipart/form-data")
+    public ResponseEntity<?> uploadBookCoverPicture(
+            @PathVariable("book-id") Integer bookId,
+            @Parameter()
+            @RequestPart("file") MultipartFile file,
+            Authentication connectedUser
+    ){
+        service.uploadBookCoverPicture(file,connectedUser, bookId),
+        return ResponseEntity.accepted().build();
+    }
+
+
 
 
     
